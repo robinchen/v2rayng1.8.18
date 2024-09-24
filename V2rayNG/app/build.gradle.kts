7bd2e864df4b29d4c32c854cebdfac051ae859e1
xiaochen <xiaochen@qq.com>
2024-09-24 13:38:28 +0800

    修改版本

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -110,7 +110,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("io.reactivex:rxjava:1.3.8")
     implementation("io.reactivex:rxandroid:1.2.1")
-    implementation("com.tbruyelle.rxpermissions:rxpermissions:0.9.4@aar")
+    implementation("com.tbruyelle.rxpermissions:rxpermissions:0.9.4")
     implementation("com.github.jorgecastilloprz:fabprogresscircle:1.01@aar")
     implementation("me.drakeet.support:toastcompat:1.1.0")
     implementation("com.blacksquircle.ui:editorkit:2.9.0")
